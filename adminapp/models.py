2fdcb3f174a37aed33f36ceb6b5cbaa5e0c9898a
shahilkhan123 <shahilkhanarimbra@gmail.com>
2022-04-22 12:16:19 +0530

    create models

--- adminapp/models.py
+++ adminapp/models.py
@@ -1,18 +1,21 @@
+
 from django.db import models
 
 
 # Create your models here.
-class Customer(models.Model):
+class Client(models.Model):
     client_name = models.CharField(max_length=30) 
-    gst_number = models.CharField(max_length=30) 
+    client_gst_number = models.CharField(max_length=30) 
     client_id = models.CharField(max_length=30) 
-    phone = models.CharField(max_length=30)
-    email = models.CharField(max_length=30)
-    state = models.CharField(max_length=30)
-    district = models.CharField(max_length=30)
-    district = models.CharField(max_length=30)
-    zipcode = models.CharField(max_length=30)
-    address = models.CharField(max_length=30)
+    client_phone = models.CharField(max_length=30)
+    client_email = models.CharField(max_length=30)
+    client_state = models.CharField(max_length=30)
+    client_district = models.CharField(max_length=30)
+    client_zipcode = models.CharField(max_length=30)
+    client_address = models.CharField(max_length=30)
+
+    class Meta:
+        db_table = 'Client'
 
 class Product(models.Model):
     food_name = models.CharField(max_length=50)
@@ -23,3 +26,31 @@ class Product(models.Model):
 
     class Meta:
         db_table = 'Product'
+
+
+class Employee(models.Model):
+    employee_username = models.CharField(max_length=50)
+    employee_password = models.CharField(max_length=50)
+    employee_phone = models.CharField(max_length=30)
+    employee_email = models.CharField(max_length=30)
+    employee_state = models.CharField(max_length=30)
+    employee_district = models.CharField(max_length=30)
+    employee_zipcode = models.CharField(max_length=30)
+    employee_address = models.CharField(max_length=30)
+
+    class Meta:
+        db_table = 'Employee'
+
+
+class AddBank(models.Model):
+    bank_name = models.CharField(max_length=50)
+    bank_holdername = models.CharField(max_length=50)
+    bank_ifsc = models.CharField(max_length=30)
+    bank_accountname = models.CharField(max_length=30)    
+    bank_balance = models.IntegerField()     
+
+    class Meta:
+        db_table = 'AddBank'
+
+
+
